139385c5003a230ab93dde1cefb1493912d05bf1
Måns Gezelius <mange61@liu.se>
2026-06-03 14:13:57 +0200

    Fix text rendering. Change font from Lato to Ubuntu. Certain fonts, including Lato, have been reported having pixelation issues in nextjs.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,18 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import { Lato } from "next/font/google"
+import { Ubuntu } from "next/font/google";
 
-
-const font = Lato({ subsets: ["latin"], weight: "400" })
+const font = Ubuntu({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+});
 
 export default function Layout({ children }: any) {
   return (
     <div className={`page-wrapper ${font.className}`}>
       <Header />
-      <main className="page-content">
-        {children}
-      </main>
+      <main className="page-content">{children}</main>
       <Footer />
     </div>
   );
